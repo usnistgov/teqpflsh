2c3e83ae7e695590e0fa339b11f7c42d2b48066c
Ian Bell <ian.bell@nist.gov>
2025-01-01 10:12:21 -0500

    Re-checkout notebooks after build

--- doc/source/sphinx_post_run.py
+++ doc/source/sphinx_post_run.py
@@ -5,8 +5,8 @@ def remove_cell_outputs():
     for path, dirs, files in os.walk('.'):
         for file in files:
             if file.endswith('.ipynb') and '.ipynb_checkpoints' not in path:
-                subprocess.check_output(f'jupyter nbconvert --clear-output --to notebook --output "{file}" "{file}"', shell=True, cwd=path)
+                subprocess.check_output(f'git checkout "{file}"', shell=True, cwd=path)
 
 if __name__ == '__main__':
-    print('running sphinx_pre_run.py...')
+    print('running sphinx_post_run.py...')
     remove_cell_outputs()
