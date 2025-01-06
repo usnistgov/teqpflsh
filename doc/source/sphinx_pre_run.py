eabc8621d4cb166b73fa9b5e9f0316d96e5fb6c8
Ian Bell <ian.bell@nist.gov>
2025-01-06 14:57:56 -0500

    Back to forcing notebooks to convert
    This shouldn't take so long...

--- doc/source/sphinx_pre_run.py
+++ doc/source/sphinx_pre_run.py
@@ -20,14 +20,15 @@ def run():
     subprocess.check_call('doxygen Doxyfile.injected', cwd=here+'/../..', shell=True)
     os.remove(newDoxyfile)
 
-    # # Execute all the notebooks to populate all cells
-    # for path, dirs, files in os.walk('.'):
-    #     for file in files:
-    #         if file.endswith('.ipynb') and '.ipynb_checkpoints' not in path:
-    #             tic = timeit.default_timer()
-    #             subprocess.check_output(f'jupyter nbconvert --allow-errors --to notebook --output "{file}" --execute "{file}"', shell=True, cwd=path)
-    #             toc = timeit.default_timer()
-    #             print(f'Elapsed for conversion: {toc-tic} seconds')
+    # Execute all the notebooks to populate all cells
+    for path, dirs, files in os.walk(here):
+        for file in files:
+            if file.endswith('.ipynb') and '.ipynb_checkpoints' not in path:
+                print(f'Converting {file} in {path}')
+                tic = timeit.default_timer()
+                subprocess.check_output(f'jupyter nbconvert --allow-errors --to notebook --output "{file}" --execute "{file}"', shell=True, cwd=path)
+                toc = timeit.default_timer()
+                print(f'Elapsed for conversion: {toc-tic} seconds')
 
 if __name__ == '__main__':
     print('running sphinx_pre_run.py...')
