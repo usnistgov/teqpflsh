b8c4863b41b2cbb891a7728effb2fa208a983c52
Ian Bell <ian.bell@nist.gov>
2025-01-06 20:33:22 -0500

    Update the post build script

--- doc/source/sphinx_post_run.py
+++ doc/source/sphinx_post_run.py
@@ -5,7 +5,8 @@ def remove_cell_outputs():
     for path, dirs, files in os.walk('.'):
         for file in files:
             if file.endswith('.ipynb') and '.ipynb_checkpoints' not in path:
-                subprocess.check_output(f'git checkout "{file}"', shell=True, cwd=path)
+                # subprocess.check_output(f'git checkout "{file}"', shell=True, cwd=path)
+                subprocess.check_output(f'jupyter nbconvert --clear-output --inplace "{file}"', shell=True, cwd=path)
 
 if __name__ == '__main__':
     print('running sphinx_post_run.py...')
